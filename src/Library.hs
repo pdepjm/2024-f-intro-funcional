22c23d0966b40f258a7ab5f96d473257b1ab1cc3
Diana López Alvas <dlopezalvas@gmail.com>
2024-04-04 11:09:29 -0300

    Ejercicio en clase con tarea

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,39 @@
 module Library where
 import PdePreludat
 
+suma :: Number -> Number -> Number
+suma x y = x + y
+
 doble :: Number -> Number
-doble numero = numero + numero
+doble x = x + x
+
+totalEmpleados :: Number -> String -> Number
+totalEmpleados cantidadSucursales nombreEmpresa = cantidadEmpleadosPorSucursal nombreEmpresa * cantidadSucursales
+
+cantidadEmpleadosPorSucursal :: String -> Number
+cantidadEmpleadosPorSucursal "acme" = 10 --Pattern matching
+
+cantidadEmpleadosPorSucursal nombreEmpresa --Guardas
+    | terminaConLetraMenor nombreEmpresa = letrasIntermedias nombreEmpresa
+    | esCapicua nombreEmpresa && nombreEsPar nombreEmpresa = letrasIntermedias nombreEmpresa * 2
+    | otherwise = 0
+
+--Tarea: Implementar funciones, y la condicion que falta (divisible por 3 o por 7)
+
+
+
+
+
+
+
+        
+
+
+
+modulo :: Number -> Number
+modulo x 
+    | x >= 0 = x
+    | x < 0 = negate x
+
+
+
